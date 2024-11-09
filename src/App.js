0cd9bd28c0be94c3aa7dfd57f6084318b35d0b38
TeddyLe68 <teddyle2003@gmail.com>
2024-11-09 23:31:47 +0700

    Challange 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { sculptureList } from "./data.js";
+import { useState } from "react";
+import "../src/App.css";
 
-function App() {
+export default function App() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  let hasPrev = index > 0;
+  let hasNext = index < sculptureList.length - 1;
+  function handlePrevClick() {
+    if (hasPrev) {
+      setIndex(index - 1);
+    }
+  }
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    }
+  }
+  function handleShowMore() {
+    setShowMore(!showMore);
+  }
+  let sculpture = sculptureList[index];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handlePrevClick} disabled={!hasPrev}>
+        Prev
+      </button>
+      <button onClick={handleNextClick} disabled={!hasNext}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name}</i> by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img className="photo" src={sculpture.url} alt={sculpture.alt} /> <br />
+      <button onClick={handleShowMore}>
+        {showMore ? "Hide" : "Show"} Details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+    </>
   );
 }
-
-export default App;
