c4a0d2989545a1b070b7d6c44d14bef4989bf46d
TeddyLe68 <teddyle2003@gmail.com>
2024-11-10 00:08:16 +0700

    challenge 4- problem

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-// Fix a crash
-// Here is a small form that is supposed to let the user leave some feedback. When the feedback is submitted, it’s supposed to display a thank-you message. However, it crashes with an error message saying “Rendered fewer hooks than expected”. Can you spot the mistake and fix it?variables instead.
+// Remove unnecessary state
+// When the button is clicked, this example should ask for the user’s name and then display an alert greeting them. You tried to use state to keep the name, but for some reason it always shows “Hello, !“.
+
+// To fix this code, remove the unnecessary state variable. (We will discuss about why this didn’t work later.)
+
+// Can you explain why this state variable was unnecessary?
 import { useState } from "react";
-import "../src/App.css";
 
 export default function FeedbackForm() {
-  const [isSent, setIsSent] = useState(false);
-  const [message, setMessage] = useState("");
-  if (isSent) {
-    return <h1>Thank you!</h1>;
-  } else {
-    // eslint-disable-next-line
+  const [name, setName] = useState("");
 
-    return (
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          alert(`Sending: "${message}"`);
-          setIsSent(true);
-        }}
-      >
-        <textarea
-          placeholder="Message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <br />
-        <button type="submit">Send</button>
-      </form>
-    );
+  function handleClick() {
+    setName(prompt("What is your name?"));
+    alert(`Hello, ${name}!`);
   }
+
+  return <button onClick={handleClick}>Greet</button>;
 }
