8a5325784312dc616e22d633b1ef1f09c60437fd
TeddyLe68 <teddyle2003@gmail.com>
2024-11-10 00:06:15 +0700

    challenge 2- solved

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 // Fix a crash
 // Here is a small form that is supposed to let the user leave some feedback. When the feedback is submitted, it’s supposed to display a thank-you message. However, it crashes with an error message saying “Rendered fewer hooks than expected”. Can you spot the mistake and fix it?variables instead.
-import { sculptureList } from "./data.js";
 import { useState } from "react";
 import "../src/App.css";
 
 export default function FeedbackForm() {
   const [isSent, setIsSent] = useState(false);
+  const [message, setMessage] = useState("");
   if (isSent) {
     return <h1>Thank you!</h1>;
   } else {
     // eslint-disable-next-line
-    const [message, setMessage] = useState("");
+
     return (
       <form
         onSubmit={(e) => {
